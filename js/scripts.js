778d5bf517de44f8063386e5d0702772d46be294
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-03-07 20:55:09 -0800

    submit function will now display each value of the array on a new list item

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
+var storeArray = [];
+
 function pingPong(userInput) {
-  for (var i = 1; i <= userInput; i += 1) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("pingpong");
+  for (var i = 1; i <= userInput; i++) {
+    if (i % 15 === 0) {
+      storeArray.push("pingpong");
     } else if (i % 5 === 0) {
-      console.log("pong");
+      storeArray.push("pong");
     } else if (i % 3 === 0 ) {
-      console.log("ping");
+      storeArray.push("ping");
     } else {
-      console.log(i);
+      storeArray.push(i);
     };
   };
+
 };
 
 
 $(document).ready(function() {
   $("form#pingPongForm").submit(function(event) {
-      event.preventDefault();
+    event.preventDefault();
 
-      var userInput = parseInt($("input#userInput").val());
-      var result = pingPong(userInput);
+    var userInput = parseInt($("input#userInput").val());
 
-      $(".listCount").text(result);
+    var result = pingPong(userInput);
+
+
+    
+      $("#listCount").append("<li>" + storeArray.splice(0, 1) + "</li>");
+
   });
 });
